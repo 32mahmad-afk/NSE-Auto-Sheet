715ea4f1e756e97e2cfecfe7f92989927ac93591
32mahmad-afk <32m.ahmad@gmail.com>
2026-07-04 22:33:27 +0530

    Update target_date to July 3, 2026

--- update_sheet.py
+++ update_sheet.py
@@ -40,7 +40,7 @@ print("🚀 Swing Institutional Scanner Started...")
 #fetched_date_str = target_date.strftime("%d-%b-%Y")
 
 #print(f"📅 Using Date: {fetched_date_str}")
-target_date = datetime(2026, 6, 30)
+target_date = datetime(2026, 7, 3)
 fetched_date_str = target_date.strftime("%d-%b-%Y")
 
 HEADERS = {
